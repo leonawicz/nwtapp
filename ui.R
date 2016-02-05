c3e67fdb7b5adfafacaed63e81cfb421a0567d5a
Matt Leonawicz <matt.rstat@gmail.com>
2016-02-05 16:38:46 -0700

    Inputs for location data

--- ui.R
+++ ui.R
@@ -8,6 +8,13 @@ shinyUI(navbarPage(theme=shinytheme("spacelab"), inverse=TRUE,
   div(class="outer",
   tags$head(includeCSS("www/styles.css")),
   leafletOutput("Map", width="100%", height="100%"),
+  selectInput("location", "Community", c("", locs$loc), selected="", multiple=F),
+  conditionalPanel("input.show_communities == true && input.location !== null && input.location !== ''",
+                   actionButton("btn_modal_loc", "View Summary", class="btn-block")),
+  bsModal("Modal_Loc", "Community Insights", "btn_modal_loc", size = "large",
+          plotOutput("TestPlot"),
+          dataTableOutput("TestTable")
+  ),
   absolutePanel(id="controls", top=360, right=0, height=300, width=300,
     conditionalPanel("input.show_colpal == true",
       wellPanel(
@@ -50,6 +57,7 @@ shinyUI(navbarPage(theme=shinytheme("spacelab"), inverse=TRUE,
   ),
   absolutePanel(bottom=10, left=10,
     conditionalPanel("input.mod_or_stat == 'Single GCM'", checkboxInput("deltas", "Display deltas", FALSE)),
+    checkboxInput("show_communities", "Show communities", TRUE),
     checkboxInput("legend", "Show legend", TRUE),
     checkboxInput("show_colpal", "Show color options", FALSE)
   )
