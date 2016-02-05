7cf8858ee3d6dc41d817967471e3a376fef7e315
Matt Leonawicz <matt.rstat@gmail.com>
2016-02-05 01:38:44 -0700

    Ensemble stats

--- server.R
+++ server.R
@@ -14,21 +14,46 @@ shinyServer(function(input, output, session) {
   })
 
   ras <- reactive({
-    x <- filter(d, Var==Variable() & RCP==input$rcp & Model==input$model)$Data[[1]]
     dec.idx <- which(decades==input$dec)
-    if(input$mon_or_sea=="Monthly"){
-      x <- x[[mon_index()]] %>% subset(dec.idx)
-    } else {
-      mon.idx <- switch(time_of_year(), Winter=c(1,2,12), Spring=3:5, Summer=6:8, Fall=9:11)
-      f <- if(Variable()=="pr") sum else mean
-      x <- calc(brick(lapply(x[mon.idx], function(x, idx) subset(x, idx), idx=dec.idx)), f)
-      x <- round(x, 1)
+    mon.idx <- switch(time_of_year(), Winter=c(1,2,12), Spring=3:5, Summer=6:8, Fall=9:11)
+    sea_func <- if(Variable()=="pr") sum else mean
+    stat_func <- switch(input$model_stats, Mean=mean,
+                        Min=function(x,...) min(x,...),
+                        Max=function(x,...) max(x,...),
+                        Spread=function(x,...) range(x,...))
+
+    mung_models <- function(x, mon_or_sea, mo, dec, mo2, f_sea){
+      if(mon_or_sea=="Monthly"){
+        x[[mo]] %>% subset(dec)
+      } else {
+        calc(brick(lapply(x[mo2], function(x, idx) subset(x, idx), idx=dec)), f_sea) %>% round(1)
+      }
     }
-    if(input$deltas & Variable()=="pr"){
-      x <- round(x / CRU_ras(), 2)
-      x[is.infinite(x)] <- NA
+
+    mung_stats <- function(x, mon_or_sea, mo, dec, mo2, f_sea, f_stat, statid){
+      if(mon_or_sea=="Seasonal") mo <- mo2
+      x <- x %>% do(., Data=.$Data[[1]][mo] %>% purrr::map(~subset(.x, dec)) %>% brick %>% calc(f_sea))
+      x <- f_stat(brick(x$Data))
+      if(statid=="Spread") x <- calc(x, function(x) x[2]-x[1])
+      round(x, 1)
     }
-    if(input$deltas & Variable()=="tas") x <- x - CRU_ras()
+
+    if(input$mod_or_stat=="Statistic"){
+      x <- filter(d, Var==Variable() & RCP==input$rcp) %>% group_by(Model, add=T) %>%
+        mung_stats(input$mon_or_sea, mon_index(), dec.idx, mon.idx, sea_func, stat_func, input$model_stats)
+      return(x)
+    }
+
+    if(input$mod_or_stat=="Single GCM"){
+      x <- filter(d, Var==Variable() & RCP==input$rcp & Model==input$model)$Data[[1]]
+      x <- mung_models(x, input$mon_or_sea, mon_index(), dec.idx, mon.idx, sea_func)
+      if(input$deltas & Variable()=="pr"){
+        x <- round(x / CRU_ras(), 2)
+        x[is.infinite(x)] <- NA
+      }
+      if(input$deltas & Variable()=="tas") x <- x - CRU_ras()
+    }
+
     x[is.nan(x)] <- NA
     x
   })
@@ -55,13 +80,13 @@ shinyServer(function(input, output, session) {
   })
 
   pal <- reactive({
-    suppressWarnings(colorNumeric(Colors(), ras_vals(), na.color="transparent"))
+    colorNumeric(Colors(), ras_vals(), na.color="transparent")
   })
 
   Legend_Title <- reactive({
     p <- input$variable=="Precipitation"
     d <- input$deltas
-    if(p & d) "Precipitation\ndeltas" else if(p) "Precipitation (mm)" else if(!p & d) "Temperature\ndeltas (C)" else "Temperature (C)"
+    if(p & d) "Precip. deltas" else if(p) "Precipitation (mm)" else if(!p & d) "Temp. deltas (C)" else "Temperature (C)"
   })
 
   output$Map <- renderLeaflet({
@@ -87,5 +112,11 @@ shinyServer(function(input, output, session) {
     tp_theme <- theme(plot.background=element_blank())
     ggplot(x, aes(`Spatial Distribution`)) + geom_density(fill="#33333350") + tp_theme + labs(x=Legend_Title())
   }, width=300, height=300, bg="transparent")
+
+  observe({
+    if(input$mod_or_stat=="Statistic"){
+      updateCheckboxInput(session, "deltas", value=FALSE)
+    }
+  })
 
 })
