611ee4fabd714c5e85120ef57e685c41db82e7d8
Matt Leonawicz <matt.rstat@gmail.com>
2016-02-05 16:38:30 -0700

    Prep for plotting and UI imrpovements, update data

--- global.R
+++ global.R
@@ -1,18 +1,21 @@
 suppressMessages({
 library(shiny)
 library(shinythemes)
+library(shinyBS)
 library(shinyjs)
 library(leaflet)
 library(raster)
 library(data.table)
 library(dplyr)
 library(tidyr)
+library(ggplot2)
 })
 
 options(warn = -1) # # haven't figured out how to suppress warning from colors(.)
 
-load("nwt_data_pr_tas_monthly_decadal_means_2010_2099.Rdata")
+load("nwt_data.RData")
 load("nwt_data_pr_tas_CRU32_1961_1990_climatology.RData")
+load("nwt_locations.RData")
 
 r <- subset(cru6190$pr, 1)
 lon <- (xmin(r)+xmax(r))/2
