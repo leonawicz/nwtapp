600a7e2f8d9263a8790a910c82ecfcb3f44874ce
Matt Leonawicz <matt.rstat@gmail.com>
2016-02-23 13:46:56 -0700

    Edit tab panels, add action button, improve reactivity

--- mod_shpPoly.R
+++ mod_shpPoly.R
@@ -8,19 +8,24 @@ shpPolyInput <- function(id, label, btn){
         )
       ),
       tabsetPanel(
-        tabPanel("Original shapefile", plotOutput(ns("Shp_Plot"), height="auto")),
-        tabPanel("Final overlay", leafletOutput(ns("Map")))
-      )
+        tabPanel("Original shapefile", plotOutput(ns("Shp_Plot"), height="auto"), value="original"),
+        tabPanel("Final overlay", leafletOutput(ns("Map")), value="final"),
+        id=ns("tp_shp")
+      ),
+      actionButton(ns("mask_btn"), "Crop and Mask to Shapefile", class="btn-block")
     )
   )
 }
 
 shpPoly <- function(input, output, session){
   ns <- session$ns
-
   userFile <- reactive({
     validate(need(input$shp_file, message=FALSE))
     input$shp_file
+  })
+  tp <- reactive({
+    validate(need(input$tp_shp, message=FALSE))
+    input$tp_shp
   })
 
   shp <- reactive({
@@ -30,22 +35,22 @@ shpPoly <- function(input, output, session){
     dir <- unique(dirname(infiles))
     outfiles <- file.path(dir, userFile()$name)
     purrr::walk2(infiles, outfiles, ~file.rename(.x, .y))
-    x <- readOGR(dir, strsplit(userFile()$name[1], "\\.")[[1]][1])
-    print(x)
-    x
+    readOGR(dir, strsplit(userFile()$name[1], "\\.")[[1]][1])
   })
 
-  shp_wgs84 <- reactive({
-    spTransform(shp(), CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
-  })
-
+  shp_wgs84 <- reactive({ spTransform(shp(), CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")) })
+  lon <- reactive({ (xmin(shp_wgs84()) + xmax(shp_wgs84()))/2 })
+  lat <- reactive({ (ymin(shp_wgs84()) + ymax(shp_wgs84()))/2 })
   plot_ht <- reactive({ if(is.null(shp())) 0 else 400 })
   output$Shp_Plot <- renderPlot({ if(!is.null(shp())) plot(shp(), col="cornflowerblue") }, height=function() plot_ht())
-  output$Map <- renderLeaflet({ leaflet() %>% addTiles() })
+  output$Map <- renderLeaflet({ leaflet() %>% setView(0, 0, zoom=2) %>% addTiles() })
 
   observe({
-    if(!is.null(shp())) leafletProxy(ns("Map")) %>% addPolygons(data=shp_wgs84())
+    if(!is.null(shp()) && tp()=="final"){
+      leafletProxy(ns("Map")) %>% clearShapes() %>% setView(lon(), lat(), zoom=2) %>% addPolygons(data=shp_wgs84(), weight=2)
+    }
   })
 
-  shp_wgs84
+  out <- reactive({ if(input$mask_btn==0) NULL else shp_wgs84 })
+  out
 }
